c50b62281e97fed46bb3e339550b4e2039b886d8
Jeremy Zheng <jitang.zheng@gmail.com>
2017-05-17 00:28:50 -0700

    create plugins

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	_ "github.com/kapmahc/fly/plugins/erp"
+	_ "github.com/kapmahc/fly/plugins/forms"
+	_ "github.com/kapmahc/fly/plugins/forum"
+	_ "github.com/kapmahc/fly/plugins/mall"
+	_ "github.com/kapmahc/fly/plugins/ops/mail"
+	_ "github.com/kapmahc/fly/plugins/ops/vpn"
+	_ "github.com/kapmahc/fly/plugins/pos"
+	_ "github.com/kapmahc/fly/plugins/reading"
+	_ "github.com/kapmahc/fly/plugins/shop"
+	_ "github.com/kapmahc/fly/plugins/site"
+)
+
+func main() {
+
+}
